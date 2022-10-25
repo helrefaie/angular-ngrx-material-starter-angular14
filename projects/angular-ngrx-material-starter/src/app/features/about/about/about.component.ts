8fe5583547baf1213f493446ebec30113f1d1a0d
Haitham Elrefaie <haitham.elrefaie@gmail.com>
2022-10-25 05:46:53 +0400

    update about page

--- projects/angular-ngrx-material-starter/src/app/features/about/about/about.component.ts
+++ projects/angular-ngrx-material-starter/src/app/features/about/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
 
 import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
 
@@ -11,8 +13,11 @@ import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
 export class AboutComponent implements OnInit {
   routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
   releaseButler = 'assets/release-butler.png';
+  angularVersion:string;
 
-  constructor() {}
+  constructor() {
+   this.angularVersion = environment.versions.angular;
+  }
 
   ngOnInit() {}
 }
